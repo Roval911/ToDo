67a6f72ea2f6dfe8efd2e5ada501e63c0c16aea5
slavarodionov <roval911@gmail.com>
2024-11-22 22:27:21 +0300

    router for api

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,34 @@
-package cmd
+package main
+
+import (
+	"ToDo/configs"
+	"ToDo/internal/controllers"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func init() {
+	// Загружаем переменные окружения из .env файла
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Инициализация базы данных
+	configs.InitDb()
+
+	// Запуск миграций
+	configs.RunMigrations()
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("api/tasks", controllers.GetTasks())
+	router.POST("api/tasks", controllers.CreateTask())
+	router.GET("api/tasks/:id", controllers.GetTaskByID())
+	router.PUT("api/tasks/:id", controllers.UpdateTask())
+	router.DELETE("api/tasks/:id", controllers.DeleteTask())
+
+	router.Run(":8080")
+}
